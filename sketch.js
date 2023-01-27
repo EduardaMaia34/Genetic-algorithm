5f51a6cef9673c1c130e3110764969cb5ba7f9c7
Eduarda Maia <eduardamfmcavalcanti@gmail.com>
2023-01-27 16:31:28 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 //global variables
 var population;
-var lifespan = 400;
+var lifespan = 200;
 var count = 0;
 var lifeInf;
 var target;
@@ -17,13 +17,15 @@ function draw() {
   background(220);
   population.run();
   lifeInf.html(count);
+  count++;
 
   if (count == lifespan) { //sempre que chegar em 200 frames, vai reinciar
+    population.evaluate();
+    population.selection();
+
     count = 0;
-    population = new Population();
   }
 
-  count++;
 
   square(target.x, target.y, 16);
   noStroke();
@@ -31,11 +33,29 @@ function draw() {
 }
 
 
-function DNA() {
-  this.genes = [];
-  for (var i = 0; i < lifespan; i++) {
-    this.genes[i] = p5.Vector.random2D();
-    this.genes[i].setMag(0.1);
+function DNA(genes) {
+  if (genes) {
+    this.genes = genes;
+  } else {
+    this.genes = [];
+    for (var i = 0; i < lifespan; i++) {
+      this.genes[i] = p5.Vector.random2D();
+      this.genes[i].setMag(0.1);
+    }
+  }
+
+  this.crossover = function(partner) {
+    var newgenes = [];
+    var mid = floor(random(this.genes.length)); //"meio" uma linha para separar
+
+    for (var i = 0; i < this.genes.length; i++) {
+      if (i > mid) {
+        newgenes[i] = this.genes[i];
+      } else {
+        newgenes[i] = partner.genes[i];
+      }
+    }
+    return new DNA(newgenes);
   }
 }
 
@@ -67,12 +87,22 @@ function Population() {
     for (var i = 0; i < this.popsize; i++) {
       var mp = this.rockets[i].fitness * 100;
       for (var j = 0; j < mp; j++) { //add to mating pool mp times
-        this.matingPool.add(this.rockets[i]);
+        this.matingPool.push(this.rockets[i]);
       }
     }
   }
 
+  this.selection = function() {
+    var newRockets = [];
+    for (var i = 0; i < this.rockets.length; i++) {
+      var parentA = random(this.matingPool).dna;
+      var parentB = random(this.matingPool).dna;
+      var child = parentA.crossover(parentB);
+      newRockets[i] = new Rocket(child);
+    }
 
+    this.rockets = newRockets;
+  }
 
   this.run = function() {
     for (var i = 0; i < this.popsize; i++) {
@@ -83,11 +113,18 @@ function Population() {
 }
 
 
-function Rocket(){
+function Rocket(dna){
   this.pos = createVector(width/2, height);
   this.vel = createVector();
   this.acc = createVector();
-  this.dna = new DNA();
+  this.count = 0;
+
+  if (dna) {
+    this.dna = dna;
+  } else {
+    this.dna = new DNA();
+  }
+
   this.fitness = 0;
 
   this.applyForce = function(force) {
@@ -101,7 +138,7 @@ function Rocket(){
   }
 
   this.update = function() {
-    this.applyForce(this.dna.genes[count]); 
+    this.applyForce(this.dna.genes[this.count]); 
     //array dna tem 200 mov, aqui vai pegar um e aplicar ao obj
 
     this.vel.add(this.acc);

4736e53bfd73e335faa41a05bc22c624126c2f3d
Eduarda Maia <eduardamfmcavalcanti@gmail.com>
2023-02-02 06:04:31 -0300

    myscript - one rocket moving
    function rocket and dna
    one rocket moving randomly
    next step: population

--- myscript.js
+++ myscript.js
@@ -1,9 +1,70 @@
-var NUM_GENES = 250;
-var VEL = 25;
-var NUM_BALLS = 100;
-var MUTATION_RATE = 1;
+var target;
+var rocket;
+var lifespan = 200;
+var count = 0;
+var lifeP;
 
-var avg_fitness = 0;
-var generation = 0;
-var balls = [];
+function setup() {
+    createCanvas(500,600);
+    target = createVector(width/2, 40);
+    rocket = new Rocket();
+    lifeP = createP();
+}
 
+function draw() {
+    background(0);
+
+    count++;
+    lifeP.html(count);
+    
+    rocket.show();
+    rocket.updateMov();
+
+    fill(color("green"));
+    square(target.x, target.y, 20);
+}
+
+function Rocket(dna) {
+    this.pos = createVector(width/2, height);
+    this.acc = createVector();
+    this.vel = createVector();
+
+    if (dna) {
+        this.dna = dna;
+    } else {
+        this.dna = new DNA();
+    }
+
+    this.applyForce = function(force) {
+        this.acc.add(force);
+    }
+
+    this.updateMov = function() {
+        this.applyForce(this.dna.genes[count]);
+
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+    }
+
+    this.show = function() {
+        push();
+        translate(this.pos.x, this.pos.y);
+        noStroke();
+        fill(color("#1B88CC"));
+        circle(0, 0, 10);
+        pop();
+    }
+}
+
+function DNA(genes){
+    if (genes) {
+        this.genes = genes;
+    } else {
+        this.genes = [];
+        for (var i = 0; i < lifespan; i++){
+            this.genes[i] = p5.Vector.random2D();
+            this.genes[i].setMag(0.5);
+        }
+    }
+}
